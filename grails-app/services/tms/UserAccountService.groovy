d817aec96e9c2ebdcfd1f2b86ced2590129e2dfe
Roberthluo <robert.otherone@gmail.com>
2015-12-02 23:04:58 -0500

    12/2/2015 commit 5
    Added class files

--- grails-app/services/tms/UserAccountService.groovy
+++ grails-app/services/tms/UserAccountService.groovy
@@ -5,10 +5,31 @@ import grails.transaction.Transactional
 @Transactional
 class UserAccountService {
     def registerInstructorAccount(String username, String password, String firstName, String lastName, String emailAddress){
+
+        try{
+            println("a");
         InstructorAccount instructorAccount = new InstructorAccount(username:username, password:password, firstName:firstName, lastName:lastName, emailAddress:emailAddress)
+
+        instructorAccount.save(failureOnError:true)
+            println("b");
     }
+
+    catch (Exception e){
+        println("v " + e);
+    }
+
+}
+
     def registerStudentAccount(String username, String password, String firstName, String lastName, String emailAddress){
+        try{
         StudentAccount studentAccount = new StudentAccount(username:username, password:password, firstName:firstName, lastName:lastName, emailAddress:emailAddress)
+
+            studentAccount.save(failureOnError:true)
+        }
+        catch (Exception e){
+            println("v " + e);
+        }
+
     }
 
 
